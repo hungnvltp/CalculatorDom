7524977ecb7c62d0d8425952992e9b222fe34cff
Hung Nguyen <hungnv8.it@gmail.com>
2020-06-25 09:47:04 +0700

    them

--- calculator.js
+++ calculator.js
@@ -2,47 +2,33 @@ let currNumber = '';
 let preNumber = '';
 let operation = '';
 let result = document.getElementById("result");
-//let equalRes = ''; // thêm dòng này 
-let dotAllow = ' không cho phép ';
+let dotAllow = true;
 let isEqual = false;
 // hiển thị số 
 function onNumber(number) {
-
     isEqual = false;
-    if (number == '.' && (dotAllow == 'đã nhập dấu chấm ' || dotAllow == ' không cho phép ')) return false;
-    if (number == '.' && dotAllow == 'cho phép ') {
-        dotAllow = 'đã nhập dấu chấm '
-    }
-    else {
-        if (dotAllow == ' không cho phép ') {
-            dotAllow = 'cho phép '
-        };
+    if (number == '.' && !dotAllow) return false;
+    if (number == '.' && dotAllow) {
+        dotAllow = false;
     }
 
     currNumber = currNumber + number;
     onShow()
 };
-
 function onOpera(opera) {
-
-    dotAllow = ' không cho phép ';
+    dotAllow = true;
     if (!currNumber) return;
     if (preNumber && ((opera && !isEqual) || (isEqual))) {
         onSave();
     }
-    // alert(currNumber);
     if (opera) operation = opera;
-    // console.log(opera);
     preNumber = currNumber;
-    // console.log(preNumber);
     if (!isEqual) currNumber = '';
     onShow();
-
 }
 
 function onSave() {
     let res = '';
-    //  equalRes = '';// thêm dòng này 
     if (!preNumber || !currNumber) return;
     currNumber = parseFloat(currNumber);
     preNumber = parseFloat(preNumber);
@@ -59,19 +45,16 @@ function onSave() {
         case '/':
             res = preNumber / currNumber;
             break;
-
     }
-    // equalRes = res;//thêm dòng này 
     currNumber = res;
-
-    //	alert(equalRes);
     operation = '';
     preNumber = '';
+    res = '';
 
 }
 // xóa 
 function onReset() {
-    dotAllow = ' không cho phép ';
+    dotAllow = true;
     currNumber = '';
     preNumber = '';
     operation = '';
@@ -81,17 +64,16 @@ function onReset() {
 function onShow() {
 
     let res = currNumber + operation;
-    // alert(res);
     if (operation && preNumber) res = preNumber + operation + currNumber;
-
     result.value = res;
-
 }
-
 // bấm dấu bằng
 function onEqual() {
+
     isEqual = true;
     onOpera();
     preNumber = '';
+    onSave();
     isEqual = false;
+
 }
